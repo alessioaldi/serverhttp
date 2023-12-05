6623ec4fd865b023d60e0d8684557a74300043ee
lai2-24 <lai2-24@itismeucci.com>
2023-12-05 13:21:32 +0100

    init

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        try {
+            ServerSocket server = new ServerSocket(8080);
+            
+
+
+
+
+
+
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
