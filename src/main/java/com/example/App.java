7472d956423abf6283cda085c5b64154ccb64995
lai2-24 <lai2-24@itismeucci.com>
2023-12-05 14:29:38 +0100

    finito

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,7 +1,16 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+import java.util.Scanner;
 
 /**
  * Hello world!
@@ -9,17 +18,62 @@ import java.net.ServerSocket;
  */
 public class App 
 {
+    private static void sendFile(PrintWriter out, String path){
+        try{
+            File myObj = new File("."+path);
+            Scanner myReader = new Scanner(myObj);
+            
+            out.println("HTTP/1.1 200 OK");
+            out.println("Content-Length: " + myObj.length());
+            out.println("Server: Java HTTP Server from Benve: 1.0");
+            out.println("Date: " + new Date());
+            out.println("Content-Type: text/html; charset=utf-8");
+
+            out.println();
+
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                out.println(data);
+                System.out.println(data);
+                
+            }
+            myReader.close();
+
+        }catch (FileNotFoundException e) {
+            out.println("HTTP/1.1 404 OK");      
+        }
+    }
+
     public static void main( String[] args )
     {
         try {
             ServerSocket server = new ServerSocket(8080);
+            while(true){
+                Socket client = server.accept();
+                BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                PrintWriter out = new PrintWriter(client.getOutputStream());
+
+                String richiesta = "";
+
+                richiesta = in.readLine();
+                String[] riga = richiesta.split(" ");
+                String path = riga[1];
+                System.out.println("--" + path + "--");
+
+                do{
+                    richiesta = in.readLine();
+                    System.out.println(richiesta);
+                    if(richiesta.isEmpty() || richiesta.equals(null)) break;
+                }while(true);
+
+
+                sendFile(out, path);
+                out.flush();
+                client.close();
+            }
+
+            // server.close();
             
-
-
-
-
-
-
 
         } catch (IOException e) {
             // TODO Auto-generated catch block
